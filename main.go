38e5b70a210e17d351a7867caaa33e0fb5f9cf01
Martin Tomes <tomesm@gmail.com>
2020-03-06 17:27:46 +0100

    Started with user input

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/tomesm/base85/pkg"
+	"os"
+	"strings"
 )
 
 func main() {
 
 	base := base85.New()
 
-	fmt.Println(base.Encode("Donec nec euismod orci"))
+	//fmt.Println(base.Encode("Lorem ipsum dolor sit amet, consectetur adipiscing elit"))
+
+	//	fmt.Println(base.Decode("6uQsS@j#Z#@j#?*Ble-0A0>f2@qboC"))
+	//
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Shell")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("hi", text) == 0 {
+			fmt.Println("hello, Yourself")
+		}
+
+	}
 
 }
--- pkg/base85.go
+++ pkg/base85.go
@@ -9,27 +9,52 @@ func New() *Base85 {
 
 func (b *Base85) Encode(text string) string {
 	source := []rune(text)
-	str := ""
-	cut := 0
+	s := ""
 
 	for len(source) > 0 {
 		if len(source) < 4 {
-			cut = len(source) + 1
-
 			for i := len(source); i < 4; i++ {
 				source = append(source, 0)
 			}
 		}
-		str += b.encodeChunk(source[:4], cut)
+		s += b.encodeChunk(source[:4])
 		source = source[4:]
 	}
-	return str
+	return s
 }
 
-func (b *Base85) encodeChunk(chunk []rune, cut int) string {
+func (b *Base85) Decode(text string) string {
+	source := []rune(text)
+	s := ""
+
+	for len(source) > 0 {
+		s += b.decodeChunk(source[:5])
+		source = source[5:]
+	}
+	//fmt.Printf("%s\n", s)
+	return s
+}
+
+func (b *Base85) decodeChunk(chunk []rune) string {
+	var decoded []rune
+	var n uint64 = uint64(chunk[0] - 33)
+
+	for i := 1; i < len(chunk); i++ {
+		mod := uint64(chunk[i]) - 33
+		n = (n * 85) + mod
+	}
+	for j := 0; j < 4; j++ {
+		decoded = append(decoded, rune(n&0xFF))
+		n = n >> 8
+	}
+	s := b.reverseRunes(string(decoded))
+	return s
+}
+
+func (b *Base85) encodeChunk(chunk []rune) string {
 	var encoded []rune
 	var n uint64 = 0
-	str := ""
+	s := ""
 
 	for _, v := range chunk {
 		n = (n << 8) | uint64(v)
@@ -40,17 +65,13 @@ func (b *Base85) encodeChunk(chunk []rune, cut int) string {
 		n = n / 85
 	}
 	for _, v := range encoded {
-		str += string(v)
+		s += string(v)
 	}
-	str = b.reverseString(str)
-
-	if cut != 0 && cut < 4 {
-		str = str[:cut]
-	}
-	return str
+	s = b.reverseRunes(s)
+	return s
 }
 
-func (b *Base85) reverseString(s string) string {
+func (b *Base85) reverseRunes(s string) string {
 	runes := []rune(s)
 
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
