d1ae83315cedae85bd3e32510a28a0642b120019
Martin Tomes <tomesm@gmail.com>
2020-04-07 22:26:30 +0200

    Removed comments

--- main.go
+++ main.go
@@ -5,29 +5,9 @@ import (
 	"github.com/tomesm/base85/pkg"
 )
 
+// Example of usage base85
 func main() {
-
 	base := base85.NewCoder()
-
 	fmt.Println(base.Encode([]rune("Lorem ipsum dolor sit amet, consectetur adipiscing elit")))
-
 	fmt.Println(base.Decode([]rune("6uQsS@j#Z#@j#?*Ble-0A0>f2@qboC")))
-
-	//reader := bufio.NewReader(os.Stdin)
-
-	//fmt.Println("Simple Shell")
-	//fmt.Println("---------------------")
-
-	//for {
-	//fmt.Print("-> ")
-	//text, _ := reader.ReadString('\n')
-	//// convert CRLF to LF
-	//text = strings.Replace(text, "\n", "", -1)
-
-	//if strings.Compare("hi", text) == 0 {
-	//fmt.Println("hello, Yourself")
-	//}
-
-	//}
-
 }
